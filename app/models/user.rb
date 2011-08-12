659001d3ee4cea3981712788feb06bf4e15ab089
Greg Smith <greg.smith@alpha-score.com>
2011-08-11 23:30:51 -0700

    added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,17 @@ class User < ActiveRecord::Base
   
   has_many :microposts, :dependent => :destroy
   
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :followers, :through => :reverse_relationships, :source => :follower
+  
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+    
+  
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, :presence => true,
@@ -50,6 +61,23 @@ class User < ActiveRecord::Base
      def feed
        # this is preliminary - see ch. 12 for rest
        Micropost.where("user_id = ?", id)
+     end
+     
+     def following?(followed)
+       relationships.find_by_followed_id(followed)
+     end
+     
+     def follow!(followed)
+       relationships.create!(:followed_id => followed.id)
+      # could also say self.relationships.create...
+     end
+     
+     def unfollow!(followed)
+       relationships.find_by_followed_id(followed).destroy
+     end
+     
+     def feed
+       Micropost.from_users_followed_by(self)
      end
 
   private
